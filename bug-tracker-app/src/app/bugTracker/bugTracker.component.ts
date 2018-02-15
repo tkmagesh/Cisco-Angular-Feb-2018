d316a7b72f83b17ecdcf65023fb928513a202c6f
Magesh <tkmagesh77@gmail.com>
2018-02-15 12:34:28 +0530

    refactoring to bugOperations service

--- bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Bug } from './models/Bug';
 
+import { BugOperationsService } from './services/bugOperations.service';
+
+
 @Component({
 	selector : 'app-bug-tracker',
 	templateUrl : 'bugTracker.component.html'
@@ -8,11 +11,28 @@ import { Bug } from './models/Bug';
 export class BugTrackerComponent{
 	bugs : Bug[] = [];
 
-	onCreateNew(bugName : string){
-		let newBug : Bug = {
-			name : bugName,
-			isClosed : false
-		};
+	/*bugOperations : BugOperationsService ;
+
+	constructor(_bugOperations : BugOperationsService){
+		this.bugOperations = _bugOperations;
+	}
+*/
+	
+	constructor(private bugOperations : BugOperationsService){
+
+	}
+	
+	onCreateNew(newBugName){
+		let newBug : Bug = this.bugOperations.createNew(newBugName);
 		this.bugs.push(newBug);
+	}
+
+	onBugClick(bugToToggle : Bug){
+		this.bugOperations.toggle(bugToToggle);
+	}
+
+	getClosedCount(){
+		console.log('getClosedCount triggered');
+		return this.bugs.reduce((result, bug) => bug.isClosed ? ++result : result, 0);
 	}
 }
