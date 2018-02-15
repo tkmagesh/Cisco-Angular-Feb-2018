8f5eb0b3be5cdec6d3a94538d9ace1398ed5c552
Magesh <tkmagesh77@gmail.com>
2018-02-15 09:48:51 +0530

    refactoring calculator result

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -5,11 +5,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GreeterComponent } from './greeter/greeter.component';
 import { CalculatorOneComponent}  from './calculator/calculatorOne.component';
+import { CalculatorTwoComponent}  from './calculator/calculatorTwo.component';
+import { CalculatorResultComponent } from './calculator/calculatorResult.component';
+
 @NgModule({
   declarations: [
     AppComponent
     , GreeterComponent
     , CalculatorOneComponent
+    , CalculatorTwoComponent
+    , CalculatorResultComponent
   ],
   imports: [
     BrowserModule
---
+++ first-app/src/app/calculator/calculatorTwo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CalculatorModel } from './CalculatorModel';
+@Component({
+	selector : 'app-calculator-two',
+	templateUrl : 'calculatorTwo.component.html'
+})
+export class CalculatorTwoComponent{
+	model :  CalculatorModel = new CalculatorModel();
+
+	operation : string = 'add';
+
+	
+}
