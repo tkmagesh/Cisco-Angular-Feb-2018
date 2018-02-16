28e4e38dc2b1e0076c2c145181ef4c4b6a52b8e2
Magesh <tkmagesh77@gmail.com>
2018-02-16 09:50:25 +0530

    bugStorage implementation

--- bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Bug } from './models/Bug';
 
-import { BugOperationsService } from './services/bugOperations.service';
+import { BugStorageService } from './services/bugStorage.service';
 
 @Component({
 	selector : 'app-bug-tracker',
@@ -10,31 +10,28 @@ import { BugOperationsService } from './services/bugOperations.service';
 export class BugTrackerComponent{
 	bugs : Bug[] = [];
 	
-	constructor(private bugOperations : BugOperationsService){
-
+	constructor(private bugStorage : BugStorageService){
+		this.bugs = this.bugStorage.getAll();
 	}
 
 	onCreateNew(newBugName){
-		let newBug : Bug = this.bugOperations.createNew(newBugName);
+		let newBug : Bug = this.bugStorage.addNew(newBugName);
 		//this.bugs.push(newBug);
 		this.bugs = [...this.bugs, newBug];
 	}
 
 	onBugClick(bugToToggle : Bug){
-		//let toggledBug = this.bugOperations.toggle(bugToToggle);
-		bugToToggle.isClosed = !bugToToggle.isClosed;
-		//this.bugs = this.bugs.map(bug => bug === bugToToggle ? toggledBug : bug);
-		this.bugs = [...this.bugs];
+		let toggledBug = this.bugStorage.toggle(bugToToggle);		
+		this.bugs = this.bugs.map(bug => bug === bugToToggle ? toggledBug : bug);
 	}
 
 	
 
 	onRemoveClosedClick(){
+		this.bugs
+			.filter(bug => bug.isClosed)
+			.forEach(closedBug => this.bugStorage.remove(closedBug));
 		this.bugs = this.bugs.filter(bug => !bug.isClosed);
 	}
 
-	getTruncatedText(str){
-		console.log('getTruncatedText triggered');
-		return str.length <= 30 ? str : str.substr(0,30) + '...';
-	}
 }
